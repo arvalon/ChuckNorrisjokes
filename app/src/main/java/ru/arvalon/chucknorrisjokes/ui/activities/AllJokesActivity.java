0d34bcaab05c8f5fd7252b661a29921a55aa75df
Artyom Vaganov <me@arvalon.ru>
2016-11-06 19:09:09 +0300

    Add refresh button

--- app/src/main/java/ru/arvalon/chucknorrisjokes/ui/activities/AllJokesActivity.java
+++ app/src/main/java/ru/arvalon/chucknorrisjokes/ui/activities/AllJokesActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -16,7 +17,6 @@ import android.widget.Toast;
 import com.arellomobile.mvp.MvpAppCompatActivity;
 import com.arellomobile.mvp.presenter.InjectPresenter;
 
-import java.util.Collection;
 import java.util.Collections;
 
 import butterknife.BindView;
@@ -38,9 +38,9 @@ public class AllJokesActivity extends MvpAppCompatActivity implements AllJokesVi
     @BindView(R.id.jokesCount)TextView jokesCount;
     @BindView(R.id.jokesRecyclerView)RecyclerView recyclerView;
     @BindView(R.id.jokeLoadProgressBar)ProgressBar jokeLoadProgressBar;
-    //@BindView(R.id.refreshJokes)Button button;
 
     private JokesAdapter jokesAdapter;
+    private boolean isListenerCreated=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,36 +80,45 @@ public class AllJokesActivity extends MvpAppCompatActivity implements AllJokesVi
 
         recyclerView.setAdapter(jokesAdapter);
 
-        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(
-                getApplicationContext(),
-                recyclerView,
-                new RecyclerTouchListener.ClickListener() {
-                    @Override
-                    public void onClick(View view, int position) {
-                        Joke joke=jokeList.getValue().get(position);
-                        Intent intent=new Intent(getApplicationContext(),JokeActivity.class);
-                        intent.putExtra("joke",joke.getJoke());
-                        startActivity(intent);
-                    }
+        if(!isListenerCreated){
+            recyclerView.addOnItemTouchListener(new RecyclerTouchListener(
+                    getApplicationContext(),
+                    recyclerView,
+                    new RecyclerTouchListener.ClickListener() {
+                        @Override
+                        public void onClick(View view, int position) {
+                            Log.d("happy","new RecyclerTouchListener.ClickListener() onClick");
+                            Joke joke=jokeList.getValue().get(position);
+                            Intent intent=new Intent(getApplicationContext(),JokeActivity.class);
+                            intent.putExtra("joke",joke.getJoke());
+                            startActivity(intent);
+                        }
 
-                    @Override
-                    public void onLongClick(View view, int position) {
-                        Toast.makeText(getApplicationContext(),
-                                "Ты что экран продавить хочешь?",
-                                Toast.LENGTH_SHORT).show();
+                        @Override
+                        public void onLongClick(View view, int position) {
+                            Toast.makeText(getApplicationContext(),
+                                    "Ты что экран продавить хочешь?",
+                                    Toast.LENGTH_SHORT).show();
+                        }
                     }
-                }
-        ));
+            ));
+
+            Log.d("happy","Listener is created");
+            isListenerCreated=true;
+        }
+    }
+
+    @Override
+    @OnClick(R.id.refreshJokesButton)
+    public void RefreshJokes() {
+
+        allJokesPresenter.getJokes();
+        Log.d("happy","@OnClick(R.id.refreshJokesButton)");
     }
 
     @Override
     public void PickUpJoke() {
 
-    }
-
-    @OnClick(R.id.refreshJokesButton)
-    void refreshJokes(){
-        allJokesPresenter.getJokes();
     }
 
 }
--- app/src/main/java/ru/arvalon/chucknorrisjokes/ui/activities/JokeActivity.java
+++ app/src/main/java/ru/arvalon/chucknorrisjokes/ui/activities/JokeActivity.java
@@ -3,6 +3,7 @@ package ru.arvalon.chucknorrisjokes.ui.activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import butterknife.BindView;
