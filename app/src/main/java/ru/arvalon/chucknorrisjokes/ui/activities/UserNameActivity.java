b90f08d5aa46b82030481c1bf4e7aaa90d2a2b41
arvalon <me@arvalon.ru>
2018-12-13 17:16:06 +0300

    master edit

--- app/src/main/java/ru/arvalon/chucknorrisjokes/ui/activities/UserNameActivity.java
+++ app/src/main/java/ru/arvalon/chucknorrisjokes/ui/activities/UserNameActivity.java
@@ -22,6 +22,7 @@ import ru.arvalon.chucknorrisjokes.vk.App;
 
 /**
  * Активность редактирования имени/фамилии пользователя
+ * master edit
  */
 public class UserNameActivity extends MvpAppCompatActivity implements UserNameView {
 
