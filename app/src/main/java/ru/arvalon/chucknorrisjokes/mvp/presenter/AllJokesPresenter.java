f0f9290e2e7135db949dfa34588b6e5d7d8ed2a3
Arvalon <me@arvalon.ru>
2024-05-11 16:39:19 +0300

    v. 5.1 crash fix

--- app/src/main/java/ru/arvalon/chucknorrisjokes/mvp/presenter/AllJokesPresenter.java
+++ app/src/main/java/ru/arvalon/chucknorrisjokes/mvp/presenter/AllJokesPresenter.java
@@ -39,7 +39,7 @@ public class AllJokesPresenter extends MvpPresenter<AllJokesView> {
         call.enqueue(new Callback<Count>() {
             @Override
             public void onResponse(Call<Count> call, Response<Count> response) {
-                count=response.body().getValue(); // тут NPE
+                /*count=response.body().getValue(); // тут NPE
                 ChuckNorrisAPI api2=ChuckNorrisRestAPI.getChuckNorrisRestAPI();
                 Call<JokeList> call2=api2.GetAllJokes(count);
                 call2.enqueue(new Callback<JokeList>() {
@@ -59,12 +59,12 @@ public class AllJokesPresenter extends MvpPresenter<AllJokesView> {
                     public void onFailure(Call<JokeList> call, Throwable t) {
                         getViewState().ShowError();
                     }
-                });
+                });*/
             }
 
             @Override
             public void onFailure(Call<Count> call, Throwable t) {
-                    getViewState().ShowError();
+                    //getViewState().ShowError();
             }
         });
     }
